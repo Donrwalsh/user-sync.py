d903c7060dbbb84ade88b22e9a653370ddc46180
Don R Walsh <donrwalsh@gmail.com>
2018-05-01 11:29:40 -0500

    Next Object, Structure Improvements
    It makes sense to combine ObjectConfig's tests with tests for its children, so in order to do that I made a utility class (in this file for now, can be broken out later) to reuse assert_eq, and other custom assertions without duplicating code.
    
    ListConfig tests will mirror ObjectConfig's, with additional considerations. I test the init in the same way, with an added assertion that a ListConfig object is actually a child of ObjectConfig - the inherited properties from the parent are not tested to avoid duplicaiton.

--- tests/object_config_test.py
+++ tests/object_config_test.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2016-2017 Adobe Systems Incorporated.  All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import unittest
 from unittest.mock import patch
 import itertools
@@ -6,11 +26,15 @@ import logging
 
 import mock
 
-from user_sync.config import ObjectConfig
+from user_sync.config import ObjectConfig, ListConfig
 from user_sync.error import AssertionException
 
 
-class ObjectConfigTest(unittest.TestCase):
+class TestUtility(unittest.TestCase):
+
+    def assert_isSubclass(self, child, parent, error_message):
+        self.assertTrue(issubclass(child, parent), error_message)
+
     def assert_eq(self, result, expected, error_message):
         """
         compares the result against the expected value, and outputs an error
@@ -19,7 +43,37 @@ class ObjectConfigTest(unittest.TestCase):
         """
         self.assertEqual(result, expected, error_message + '\nexpected: %s, got: %s' % (expected, result))
 
+
+class ListConfigTest(unittest.TestCase):
     def setUp(self):
+        self.util = TestUtility()
+        self.primary_scope = "primary"
+        self.secondary_scope = "secondary"
+        self.primary_values = ["one", "two", "three"]
+        self.secondary_values = ["four", "five", "six"]
+        self.test_object_primary = ListConfig(self.primary_scope, self.primary_values)
+        self.test_object_secondary = ListConfig(self.secondary_scope, self.secondary_values)
+
+    def test_init(self):
+        try:
+            ListConfig()
+        except TypeError:
+            pass
+        except Exception as e:
+            self.fail('Unexpected exception raised:', e)
+        else:
+            self.fail("TypeError not raised when required arguments are omitted")
+
+        self.util.assert_eq(self.test_object_primary.value, self.primary_values,
+                            "On init, passed value should not change")
+        self.util.assert_isSubclass(self.test_object_primary.__class__, ObjectConfig,
+                                    "ListConfig objects should be children of ObjectConfig")
+
+
+class ObjectConfigTest(unittest.TestCase):
+
+    def setUp(self):
+        self.util = TestUtility()
         self.primary_scope = "primary"
         self.secondary_scope = "secondary"
         self.test_object_primary = ObjectConfig(self.primary_scope)
@@ -35,60 +89,60 @@ class ObjectConfigTest(unittest.TestCase):
         else:
             self.fail("TypeError not raised when scope is omitted")
 
-        self.assert_eq(self.test_object_primary.parent, None,
-                       "On init, parent should be None")
-        self.assert_eq(self.test_object_primary.child_configs, {},
-                       "On init, child_configs should be an empty dictionary")
-        self.assert_eq(self.test_object_primary.scope, self.primary_scope,
-                       "On init, passed scope should not change")
+        self.util.assert_eq(self.test_object_primary.parent, None,
+                            "On init, ObjectConfig.parent should be None")
+        self.util.assert_eq(self.test_object_primary.child_configs, {},
+                            "On init, child_configs should be an empty dictionary")
+        self.util.assert_eq(self.test_object_primary.scope, self.primary_scope,
+                            "On init, passed scope should not change")
 
     def test_set_parent(self):
         self.test_object_primary.set_parent(self.test_object_secondary)
-        self.assert_eq(self.test_object_primary.parent, self.test_object_secondary,
-                       "Object added as parent should not change")
+        self.util.assert_eq(self.test_object_primary.parent, self.test_object_secondary,
+                            "Object added as parent should not change")
 
     def test_add_child(self):
         self.test_object_primary.add_child(self.test_object_secondary)
 
-        self.assert_eq(self.test_object_primary.child_configs[self.secondary_scope], self.test_object_secondary,
-                       "Object added as child should not change")
-        self.assert_eq(self.test_object_secondary.parent, self.test_object_primary,
-                       "Child object should contain unchanged parent object")
+        self.util.assert_eq(self.test_object_primary.child_configs[self.secondary_scope], self.test_object_secondary,
+                            "Object added as child should not change")
+        self.util.assert_eq(self.test_object_secondary.parent, self.test_object_primary,
+                            "Child object should contain unchanged parent object")
 
     def test_find_child_config(self):
         self.test_object_primary.add_child(self.test_object_secondary)
 
-        self.assert_eq(self.test_object_primary.find_child_config(self.secondary_scope), self.test_object_secondary,
-                       "Object added as child should be findable in child_configs attribute")
+        self.util.assert_eq(self.test_object_primary.find_child_config(self.secondary_scope), self.test_object_secondary,
+                            "Object added as child should be findable in child_configs attribute")
 
         self.test_object_primary.add_child(self.test_object_secondary)
 
-        self.assert_eq(self.test_object_primary.find_child_config(self.secondary_scope), self.test_object_secondary,
-                       "find_child_configs should only return one object even if there are more than one valid children")
+        self.util.assert_eq(self.test_object_primary.find_child_config(self.secondary_scope), self.test_object_secondary,
+                            "find_child_configs should only return one object even if there are multiple valid children")
 
     def test_iter_configs(self):
         for object_config in self.test_object_primary.iter_configs():
-            self.assert_eq(object_config, self.test_object_primary,
-                           "iter_configs() of an object config with no hierarchy should only return itself")
+            self.util.assert_eq(object_config, self.test_object_primary,
+                                "iter_configs() of an object config with no hierarchy should only return itself")
 
         self.test_object_primary.add_child(self.test_object_secondary)
         self.test_object_primary.add_child(self.test_object_secondary)
 
         for object_config in itertools.islice(self.test_object_primary.iter_configs(), 1):
-            self.assert_eq(object_config, self.test_object_primary,
-                           "iter_configs() of an object config with children should return itself in first position")
+            self.util.assert_eq(object_config, self.test_object_primary,
+                                "iter_configs() of an object config with children should return itself in 1st position")
 
         for object_config in itertools.islice(self.test_object_primary.iter_configs(), 1, 3):
-            self.assert_eq(object_config, self.test_object_secondary,
-                           "iter_configs() of an object config with children should contain those children")
+            self.util.assert_eq(object_config, self.test_object_secondary,
+                                "iter_configs() of an object config with children should contain those children")
 
     def test_get_full_scope(self):
         self.test_object_primary.add_child(self.test_object_secondary)
 
-        self.assert_eq(self.test_object_secondary.get_full_scope(), "primary.secondary",
-                       "child object's full scope should include parent and child")
-        self.assert_eq(self.test_object_primary.get_full_scope(), "primary",
-                       "parent object's full scope should only include self")
+        self.util.assert_eq(self.test_object_secondary.get_full_scope(), "primary.secondary",
+                            "child object's full scope should include parent and child")
+        self.util.assert_eq(self.test_object_primary.get_full_scope(), "primary",
+                            "parent object's full scope should only include self")
 
     def test_create_assertion_error(self):
         try:
@@ -100,22 +154,20 @@ class ObjectConfigTest(unittest.TestCase):
 
     def test_describe_types(self):
         if six.PY3:
-            self.assert_eq(self.test_object_primary.describe_types(str), ['str'],
-                           'python 3 should describe strings as str')
-            self.assert_eq(self.test_object_primary.describe_types(six.string_types), ['str'],
-                           'python 3 should describe six.string_types as str')
-            self.assert_eq(self.test_object_primary.describe_types(dict), ['dict'],
-                           'python 3 should describe dictionaries as dict')
-            self.assert_eq(self.test_object_primary.describe_types(list), ['list'],
-                           'python 3 should describe lists as list')
-            self.assert_eq(self.test_object_primary.describe_types((int, bool, tuple)), ['int', 'bool', 'tuple'],
-                           'python 3 should describe multiple types in an ordered list')
+            self.util.assert_eq(self.test_object_primary.describe_types(str), ['str'],
+                                'python 3 should describe strings as str')
+            self.util.assert_eq(self.test_object_primary.describe_types(six.string_types), ['str'],
+                                'python 3 should describe six.string_types as str')
+            self.util.assert_eq(self.test_object_primary.describe_types(dict), ['dict'],
+                                'python 3 should describe dictionaries as dict')
+            self.util.assert_eq(self.test_object_primary.describe_types(list), ['list'],
+                                'python 3 should describe lists as list')
+            self.util.assert_eq(self.test_object_primary.describe_types((int, bool, tuple)), ['int', 'bool', 'tuple'],
+                                'python 3 should describe multiple types in an ordered list')
 
     @patch.object(ObjectConfig, "describe_unused_values")
-    @patch.object(ObjectConfig, "iter_configs")
-    def test_report_unused_values(self, iter_configs_mock, describe_unused_values_mock):
-        return_configs = {self.test_object_secondary}
-        iter_configs_mock.return_value = return_configs
+    def test_report_unused_values(self, describe_unused_values_mock):
+        self.test_object_primary.add_child(self.test_object_secondary)
         m_logger = mock.Mock()
 
         # The core ObjectConfig should never log with this method as describe_unused_values returns nothing
@@ -133,7 +185,8 @@ class ObjectConfigTest(unittest.TestCase):
 
         describe_unused_values_mock.return_value = "key_in_optional_configs"
         try:
-            self.test_object_primary.report_unused_values(m_logger, [self.test_object_secondary])
+            self.test_object_primary.report_unused_values(m_logger,
+                                                          [self.test_object_primary, self.test_object_secondary])
         except AssertionException:
             self.fail("An AssertionException should not be raised if an unused key is present in optional configs")
         else:
@@ -141,13 +194,6 @@ class ObjectConfigTest(unittest.TestCase):
             pass
 
     def test_describe_unused_values(self):
-        self.assert_eq(self.test_object_primary.describe_unused_values(), [],
-                       "This method should return an empty list")
-
-
-
-
-
-
-
+        self.util.assert_eq(self.test_object_primary.describe_unused_values(), [],
+                            "This method should return an empty list")
 
